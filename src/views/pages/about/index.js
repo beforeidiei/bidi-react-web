7454d926b3523dbd58a065e266a7ab79d3bdb9f5
erictherobot <erictherobot@gmail.com>
2016-10-05 12:57:17 -0400

    Simplifying the about page

--- src/views/pages/about/index.js
+++ src/views/pages/about/index.js
@@ -6,11 +6,8 @@ const About = () => {
       <div className="g-col">
         <h3 className="about__sub-heading">What is BIDI?</h3>
         <p>Before I Die I (BIDI) is a personalized bucket list of things to do before you die. </p>
-        <h3 className="about__sub-heading">Why was BIDI created?</h3>
-        <p>As humans, our only hope is that the sun will rise tomorrow. However there are no guarantees in life. Anything could happen. BIDI was created to encourage you to write your ideas, thoughts, goals, dreams, and visions down. Our platform emails and encourages you periodically to get busy living and cross these items off your lists. You control your lists and the timelines associated with them. We just hold you accountable.</p>
-        <h3 className="about__sub-heading">How did BIDI begin?</h3>
-        <p>BIDI was first introduced to the internet in 2008 by Eric David Smith. The initial concept was anonymous and public facing. There were no personalized bucket lists in the beginning, only a single list displayed on our home page.</p>
-        <p>After realizing that some bucket lists were sensitive and private, we decided to rethink our product and ultimately decided to take the web site down for several years. The project resurfaced in October 2016 and we believe we will be able to better assist people in achieving their goals.</p>
+        <h3 className="about__sub-heading">How it works</h3>
+        <p>BIDI is incredibly simple to use. Just sign in with your Twitter account and you'll be presented with a new bucket list. From here you can create your lists and start checking them off your bucket list. </p>
       </div>
     </div>
   );
